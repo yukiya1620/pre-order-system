a2cf75abe067b456ba385ad2afce10a7c59f572a
yukiy <yukiya162003@yahoo.co.jp>
2026-07-17 08:07:04 +0900

    chore: remove unused default API route

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,7 @@ use App\Http\Controllers\Api\V1\OrderController;
 use App\Http\Controllers\Api\V1\ProductController;
 use App\Http\Controllers\Api\V1\SmsAuthController;
 use App\Http\Controllers\Api\V1\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
     Route::prefix('auth')->group(function () {
